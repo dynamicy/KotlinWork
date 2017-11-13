f19b7ef2dc956b961e76396a0c954d224cf6a2b0
dynamicy <fermat.little.theorem@gmail.com>
2017-11-13 22:22:04 +0800

    Basic concept of Kotlin.

--- HelloWorld/src/MyFirstKotlin.kt
+++ HelloWorld/src/MyFirstKotlin.kt
@@ -1,3 +1,27 @@
-fun main(args:Array<String>) {
-    print("Welcome to Kotlin!")
+fun main(args: Array<String>) {
+
+    /**
+     * Basic Output
+     */
+    print("Welcome to Kotlin!\n")
+
+    print(100)
+
+    println()
+
+    /**
+     * Variable
+     */
+    var name: String = "Chris"
+
+    var description: String
+
+    var age: Int = 10
+
+    var tall: Double = 170.8
+
+    description = " is a nerd!"
+
+    print(name + description + " Age: " + age + ", tall: " + tall + " cm.")
+
 }
