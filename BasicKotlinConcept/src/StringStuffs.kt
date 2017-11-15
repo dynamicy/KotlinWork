868dc4ea2d69b6821b43a342cb56b008b12dd67a
dynamicy <fermat.little.theorem@gmail.com>
2017-11-15 19:51:59 +0800

    String stuff.

---
+++ BasicKotlinConcept/src/StringStuffs.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+    val demoStr = " this is an apple. "
+
+    // trim()
+    println(demoStr.trim())
+
+    // split
+    println(demoStr.split(" "))
+
+    // toLowerCase
+    println(demoStr.toLowerCase())
+
+    // toUpperCase
+    println(demoStr.toUpperCase())
+
+    // plus
+    println(demoStr.plus("banana"))
+
+    // replace
+    println(demoStr.replace(" ", "-"))
+}
