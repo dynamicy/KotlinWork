b6ba061ad03ff5ef8a2ccc114d208e3fc2a5eafe
dynamicy <fermat.little.theorem@gmail.com>
2017-11-15 16:55:05 +0800

    Return and jump.

---
+++ BasicKotlinConcept/src/ReturnNJumps.kt
@@ -0,0 +1,27 @@
+/**
+ * Return and Jump
+ */
+fun main(args: Array<String>) {
+
+    for (x in 1..10) {
+        for (y in 1..10) {
+            if (x == 4 && y == 5) {
+                break
+            }
+            print(" " + x * y + " ")
+        }
+        println()
+    }
+
+    println()
+
+    loop1@ for (x in 1..10) {
+        for (y in 1..10) {
+            if (x == 4 && y == 5) {
+                break@loop1
+            }
+            print(" " + x * y + " ")
+        }
+        println()
+    }
+}
