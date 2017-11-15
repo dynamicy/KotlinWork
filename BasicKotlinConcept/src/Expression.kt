f83ca01d9daf73b58fb4b9733adc4a5a621ab95c
dynamicy <fermat.little.theorem@gmail.com>
2017-11-15 16:12:21 +0800

    Expression.

---
+++ BasicKotlinConcept/src/Expression.kt
@@ -0,0 +1,19 @@
+/**
+ * Expression
+ */
+fun main(args: Array<String>) {
+
+    val n1: Int = readLine()!!.toInt()
+    val n2: Int = readLine()!!.toInt()
+
+    var max = if (n1 > n2) n1 else n2
+
+    println("Max: $max")
+
+    when (n1 > n2) {
+        true -> max = n1
+        else -> max = n2
+    }
+
+    println("Max: $max")
+}
