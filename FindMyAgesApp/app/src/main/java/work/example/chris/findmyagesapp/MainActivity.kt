75b2faf10f7f26c42c6ba320b5ff315019e2f962
dynamicy <fermat.little.theorem@gmail.com>
2017-11-20 23:16:42 +0800

    Update layout.

--- FindMyAgesApp/app/src/main/java/work/example/chris/findmyagesapp/MainActivity.kt
+++ FindMyAgesApp/app/src/main/java/work/example/chris/findmyagesapp/MainActivity.kt
@@ -2,11 +2,19 @@ package work.example.chris.findmyagesapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val getMyAgeBtn = findViewById<Button>(R.id.getMyAgeBtn)
+
+        getMyAgeBtn.setOnClickListener {
+//            val yearOfBirth = yearEditText.text
+        }
     }
 }
