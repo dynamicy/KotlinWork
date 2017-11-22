4f38b3c07b067b37886af148534cc77d49972149
dynamicy <fermat.little.theorem@gmail.com>
2017-11-22 18:14:28 +0800

    Generate table dynamically.

--- TicTacToe/app/src/main/java/work/example/chris/tictactoe/MainActivity.kt
+++ TicTacToe/app/src/main/java/work/example/chris/tictactoe/MainActivity.kt
@@ -1,33 +1,49 @@
 package work.example.chris.tictactoe
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.view.Gravity
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TableLayout
+import android.widget.TableRow
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        init(3)
     }
 
-    fun onBtnClick(view: View) {
-        val btn: Button = view as Button
-        var btnId = 0
-        when (btn.id) {
-            R.id.btn1 -> btnId = 1
-            R.id.btn2 -> btnId = 2
-            R.id.btn3 -> btnId = 3
-            R.id.btn4 -> btnId = 4
-            R.id.btn5 -> btnId = 5
-            R.id.btn6 -> btnId = 6
-            R.id.btn7 -> btnId = 7
-            R.id.btn8 -> btnId = 8
-            R.id.btn9 -> btnId = 9
+    fun init(rowNum: Int) {
+
+        val gameBoard: TableLayout = findViewById(R.id.gameBoard)
+
+        for (i in 0..rowNum) {
+            // Set table row
+            val tableRow = TableRow(this)
+            val layoutParam: TableRow.LayoutParams = TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT)
+            layoutParam.weight = 1f
+            layoutParam.setMargins(1,1,1,1)
+            layoutParam.gravity = Gravity.CENTER_HORIZONTAL
+            tableRow.layoutParams = layoutParam
+
+            for (j in 0..rowNum) {
+                // Set content
+                val cellButton = Button(this)
+                cellButton.height = 40
+                cellButton.width = 40
+                cellButton.text = j.toString()
+                cellButton.textSize = 18f
+                cellButton.setBackgroundColor(Color.GREEN)
+
+                tableRow.addView(cellButton)
+            }
+            gameBoard.addView(tableRow, i)
+
         }
-        Toast.makeText(this, "ID:" + btnId, Toast.LENGTH_SHORT).show()
     }
 
 }
