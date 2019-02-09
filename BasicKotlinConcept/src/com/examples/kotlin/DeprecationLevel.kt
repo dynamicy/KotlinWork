386761e90141f47af988344591b52aa72c20319e
Chris <chris@csie.io>
2019-02-09 23:34:34 +0800

    DeprecationLevel.

--- BasicKotlinConcept/src/com/examples/kotlin/DeprecationLevel.kt
+++ BasicKotlinConcept/src/com/examples/kotlin/DeprecationLevel.kt
@@ -1,10 +1,17 @@
 package com.examples.kotlin
 
-@Deprecated("retired", ReplaceWith("println(\"Do something\")"), level = DeprecationLevel.WARNING)
+@Deprecated("retired", level = DeprecationLevel.WARNING)
 fun doSomething() {
     println("Do something")
 }
 
+@Deprecated("replace", ReplaceWith("Joiner.on(sep).join(strings)", imports = ["com.google.common.base.Joiner"]))
+fun doAction() {
+    println("Do action")
+}
+
 fun main(args: Array<String>) {
     doSomething()
+
+    doAction()
 }
