1a3f58765adb67c318ab45cf117c29b386261012
Chris <chris@csie.io>
2019-02-09 13:07:49 +0800

    Default arguments

---
+++ BasicKotlinConcept/src/com/examples/kotlin/jam/DefaultArguments.kt
@@ -0,0 +1,27 @@
+package com.examples.kotlin.jam
+
+fun foo(name: String, number: Int): String {
+    return foo(name, number, false)
+}
+
+fun foo(name: String, toUpperCase: Boolean): String {
+    return foo(name, 42, toUpperCase)
+}
+
+fun foo(name: String): String {
+    return foo(name, 42)
+}
+
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.toUpperCase() else name) + number
+
+fun useFoo() = listOf(
+        foo("a"),
+        foo("b", number = 1),
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
+)
+
+fun main() {
+    println(useFoo())
+}
